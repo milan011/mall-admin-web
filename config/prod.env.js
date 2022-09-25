e1ccab9dd96b955ad6ba34ec3a81721c26d35951
wcgInHome <milan011@sohu.com>
2022-09-25 23:34:15 +0800

    build箭头处理

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"https://admin-api.macrozheng.com"'
+  //BASE_API: '"https://admin-api.macrozheng.com"'
+  BASE_API: '"https://guoda365.erpwm.com/api"'
 }
